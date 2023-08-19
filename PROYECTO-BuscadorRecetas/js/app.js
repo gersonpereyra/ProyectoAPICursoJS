6dba1ac8ca9d0662e31bc4c93fc642779ee738c6
GersonPereyra <ghernandezpereyra@gmail.com>
2023-08-19 15:09:34 -0600

    Modales agregados

--- PROYECTO-BuscadorRecetas/js/app.js
+++ PROYECTO-BuscadorRecetas/js/app.js
@@ -4,6 +4,7 @@ function iniciarApp(){
     selectCategorias.addEventListener('change', seleccionarCategoria);
 
     const resultado = document.querySelector('#resultado');
+    const modal = new bootstrap.Modal('#modal', {});
 
     obtenerCategorias();
 
@@ -63,7 +64,11 @@ function iniciarApp(){
             const recetaButton = document.createElement('BUTTON');
             recetaButton.classList.add('btn', 'btn-danger', 'w-100');
             recetaButton.textContent = 'Ver Receta';
-
+            recetaButton.dataset.bsTarget = "#modal";
+            recetaButton.dataset.bsToggle = "modal";
+            recetaButton.onclick = function (){
+                seleccionarReceta(idMeal)
+            }
 
             console.log(recetaImagen);
 
@@ -76,6 +81,64 @@ function iniciarApp(){
             recetaContenedor.appendChild(recetaCard);
             resultado.appendChild(recetaContenedor)
         });
+    }
+
+    function seleccionarReceta(id){
+        const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarRecetaModal(resultado.meals[0]))
+    }
+
+    function mostrarRecetaModal(receta) {
+        //mostrar modal
+        const {idMeal, strInstructions, strMeal, strMealThumb } = receta;
+
+        const modalTitle = document.querySelector('.modal .modal-title');
+        const modalBody = document.querySelector('.modal .modal-body');
+        modalTitle.textContent = strMeal;
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb }" alt="receta ${strMeal}" />
+            <h3 class="my-3">Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3 class="my-3">Ingredientes y Cantidades</h3>
+        `;
+
+        const listGroup = document.createElement('UL');
+        listGroup.classList.add('list-group');
+        //mostrar cantidades e ingredientes
+        for(let i = 1;i <= 20; i++){
+            if(receta[`strIngredient${i}`]){
+                const ingrediente = receta[`strIngredient${i}`];
+                const cantidad = receta[`strMeasure${i}`];
+
+                const ingredienteLi = document.createElement('LI');
+                ingredienteLi.classList.add('list-group-item');
+                ingredienteLi.textContent = `${ingrediente} - ${cantidad}`;
+            
+                listGroup.appendChild(ingredienteLi);
+            }
+        }
+        modalBody.appendChild(listGroup);
+        
+        const modalFooter = document.querySelector('.modal-footer');    
+        limpiarHtml(modalFooter);
+        //Botones de cerrar y favorito
+        const btnFavorito = document.createElement('BUTTON'); 
+        btnFavorito.classList.add('btn', 'btn-danger', 'col');
+        btnFavorito.textContent= 'Guardar Favorito';
+
+        const btnCerrar = document.createElement('BUTTON'); 
+        btnCerrar.classList.add('btn', 'btn-secondary', 'col');
+        btnCerrar.textContent= 'Cerrar';
+        btnCerrar.onclick = function() {
+            modal.hide();
+        }
+
+        modalFooter.appendChild(btnFavorito);
+        modalFooter.appendChild(btnCerrar);
+        //muestra el modal
+        modal.show();
     }
 
     function limpiarHtml(selector){
